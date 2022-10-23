0b6cdf780ebf37b6fe63a42c0f796b99322eb05d
Rich-Tech-18 <R.Techer@gmail.com>
2022-10-23 05:23:25 +0200

    Ajout du composant RadarCharts sans intégration

---
+++ front/src/compenents/RadarCharts/RadarCharts.jsx
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
+import './RadarCharts.css';
+
+
+const RadarCharts = () => {
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" />
+          <PolarRadiusAxis />
+          <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+        </RadarChart>
+      </ResponsiveContainer>
+    )
+}
+
+
+export default RadarCharts;
